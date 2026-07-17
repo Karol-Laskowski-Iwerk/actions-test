89864416d43e1b83171640d66d37a1acb4446e60
Karol Laskowski <klaskowski@iwerk.com>
2026-07-17 13:01:41 -0400

    fixed test

--- tests/heartbeat.spec.ts
+++ tests/heartbeat.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-const pagesToCheck = ['/'];
+const pagesToCheck = ['https://google.com'];
 
 for (const path of pagesToCheck) {
   test(`@heartbeat page ${path} opens successfully`, async ({ page }) => {
     const response = await page.goto(path);
     expect(response?.status()).toBeLessThan(400);
     await expect(page).toHaveTitle("Google");
-    await expect(page.locator('input[aria-label="Google Search"]')).toBeVisible();
+    await expect(page.locator('input[value="I\'m Feeling Lucky"]:not([jsaction])')).toBeVisible();
   });
 }
